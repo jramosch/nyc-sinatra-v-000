05428cdf0813ba909af11619b0822c2f86ffb221
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-05-10 23:06:13 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -17,13 +17,13 @@ class FiguresController < ApplicationController
 
   post '/figures' do
     @figure = Figure.create(params[:figure])
-    title = Title.find_or_create_by(params["title"]["name"])
     if !params["title"]["name"].empty?
       @figure.titles << Title.create(params["title"]["name"])
     end
     if !params["landmark"]["name"].empty?
       @figure.landmarks << Landmark.create(params[:landmark])
     end
+    binding.pry
     redirect to "/figures/#{@figure.id}"
   end
 
